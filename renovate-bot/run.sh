5cbe99859bf76a4f00d27cfe6303d17cc891b7d9
belly-rewardz <dekri@rewardz.sg>
2026-01-10 11:22:27 +0700

    Enhance run.sh to include debug logging and additional environment variables

--- renovate-bot/run.sh
+++ renovate-bot/run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 TOKEN=$(cat github-pat.txt)
-docker run --rm \
+docker run --rm -i \
   -v "$(pwd)/config.js:/usr/src/app/config.js" \
   -e RENOVATE_TOKEN="$TOKEN" \
-  renovate/renovate:42.76 
+  -e LOG_LEVEL="debug" \
+  renovate/renovate:42.76
 
-# --dry-run=full
+  # -e RENOVATE_FETCH_CHANGELOGS="disabled" \
+  # -e RENOVATE_FETCH_RELEASE_NOTES="disabled" \
+# Remove --dry-run=full after testing
+# Change LOG_LEVEL to "debug" for detailed logs
